c61628486d659416ec3adda88851c0bb758ce435
Ahmed karim <121400813+Muhammadahmed2001@users.noreply.github.com>
2023-03-09 16:08:46 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,60 @@
+// var arr = [100,5,10,45,25,60,70,30,80,90,95,65]
+// small = arr[0];
+// for(i = 0 ; i < arr.length;i++){
+//     if (small < arr[i]){
+//         small = arr[i]
+//     }
+// }
+// document.write(small);
+
+
+
+
+// var arr = [21, 22, 23, 25, 26, 27, 28, 29, 30];
+// missnum = 0;
+// for (i = 0; i < arr.length - 1; i++) {
+//     if (arr[i] ==! arr[i + 1] - 1) {
+//         missnum = arr[i] + 1;
+//     }
+// }
+// document.write(missnum);
+
+
+// var str = "muhammad ahmed";
+// console.log(str[0].toUpperCase() + str.slice(1,9) + str[9].toUpperCase() + str.slice(10));
+
+
+// var str = prompt("Enter your name");
+// var word = str.split(" ");
+// var arr = [];
+// // console.log(str[0].toUpperCase()+str.slice(1));
+// for (var i = 0; i < word.length; i++) {
+//     var result = word[i][0].toUpperCase() + word[i].slice(1)
+//     arr.push(result);
+// }
+
+// console.log(arr.join(" "));
+
+
+
+// var str = prompt("Enter your name");
+// var arr = str.split(" ");
+// result = [];
+// for (var i = 0; i < arr.length; i++){
+//     capital = arr[i][0].toUpperCase() + arr[i].slice(1)
+//     result.push(capital);
+// }
+
+// console.log(result.join(" "));
+
+
+
+
+
+
+
+
+
+
+
+
